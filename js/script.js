48c063147b31ed4127f3ab3d0d25c5013e03f873
Chin <amifunny27@gmail.com>
2020-03-09 15:01:52 -0400

    added search functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-//First figure out the caption
-//If a specific caption title/letter isn't the value of the search
-//Then hide those values 
-//$('.gallery-1 a[data-title="My caption"]').hide(); 
+// Search Results //
+
+$(document).ready(function() {
+    $("#search").on("keyup", function() {
+        //Target input element and pass event listener "keyup"
+        let value = $(this) //case sensitive
+            .val()
+            .toLowerCase();
+        $("a").each(function() {
+            //loop through all link elements on page
+            let str = $(this) //targeting the data-title attribute
+                .attr("data-title")
+                .toLowerCase();
+            if (str.indexOf(value) > -1) {
+                //if the data title attribute contains a letter in value/input
+                $(this).show(); //show all possible options
+            } else {
+                $(this).hide(); //hide all possible options
+            }
+        });
+    });
+});
