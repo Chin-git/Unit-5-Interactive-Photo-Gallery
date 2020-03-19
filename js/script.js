fbbbe5014d938bbba7903bb6b71bb26828abafa8
Chin <amifunny27@gmail.com>
2020-03-19 10:50:53 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 $(document).ready(function() {
     $("#search").on("keyup", function() {
         //Target input element and pass event listener "keyup"
-        let value = $(this).val().toLowerCase(); //case sensitive  
+        let value = $(this)
+            .val()
+            .toLowerCase(); //case sensitive
         $("a").each(function() {
             //loop through all link elements on page
             let str = $(this).attr("data-title"); //targeting the data-title attribute
